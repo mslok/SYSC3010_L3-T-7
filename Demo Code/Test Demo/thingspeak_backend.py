2f7fd99a015eba97b707fe55dd07107a1a7a90e7
alextasseron <alextasseron@cmail.carleton.ca>
2020-11-17 16:17:29 -0500

    added skeleton functions

--- Demo Code/Test Demo/thingspeak_backend.py
+++ Demo Code/Test Demo/thingspeak_backend.py
@@ -1,2 +1,59 @@
-#CODE GOES HERE
+import http.client
+import urllib.parse
+import requests
+import json
 
+def getData(api_key,channel_id):
+#Pulls data from a Thingspeak channel.	
+	return 0
+
+def sendData():
+	return 0
+
+def getAdress(): 
+	return 0
+
+def readFile():
+	return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	return 0
+
+if __name__ == "__main__":
+	main()
